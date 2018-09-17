cf881a4029f0a4dad3c9fa505a180cf123911786
HyunJungByun <wowyowowyo@gmail.com>
2018-09-17 13:06:46 +0900

    Update exam_01.js

--- ch02/2.1.5/exam_01.js
+++ ch02/2.1.5/exam_01.js
@@ -1,1 +1,27 @@
 
+var candyMachine = {
+  status: {
+    name: 'node',
+    count: 5,
+  },
+  getCandy: function() {
+    this.status.count--;
+    return this.status.count;
+  }
+};
+var getCandy = candyMachine.getCandy;
+var count = candyMachine.status.count;
+
+// 아래와 같이 변경 가능
+
+const candyMachine = {
+  status: {
+    name: 'node',
+    count: 5,
+  },
+  getCandy() {
+    this.status.count--;
+    return this.status.count;
+  }
+};
+const { getCandy, status: { count } } = candyMachine;
