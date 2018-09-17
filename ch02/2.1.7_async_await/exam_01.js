7fa2b8c3d89ec4c7fc3f867448872177d0f0bf64
HyunJungByun <wowyowowyo@gmail.com>
2018-09-17 13:48:47 +0900

    Update exam_01.js

--- ch02/2.1.7_async_await/exam_01.js
+++ ch02/2.1.7_async_await/exam_01.js
@@ -1,1 +1,26 @@
 
+async function findAndSaveUser(Users) {
+  try {
+  let user = await Users.findOne({});
+  user.name = 'zero';
+  user = await user.save();
+  user = await Users.findOne({ gender: 'm' });
+  
+  }catch (error) {
+    console.error(error);
+  }
+}
+
+// 아래와 같이 변경 할 수 있다.
+
+const findAndSaveUser = async (Users) => {
+  try {
+    let user = await Users.findOne({});
+    user.name = 'zero';
+    user = await user.save();
+    user = await Users.findOne({ gender: 'm' });
+    
+  } catch (error) {
+    console.error(error);
+  }
+};
