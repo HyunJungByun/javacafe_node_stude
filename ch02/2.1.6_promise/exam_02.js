0f02372d2c35619d2c1f41dd8643a28b7803c599
HyunJungByun <wowyowowyo@gmail.com>
2018-09-17 13:38:07 +0900

    Update exam_02.js

--- ch02/2.1.6_promise/exam_02.js
+++ ch02/2.1.6_promise/exam_02.js
@@ -1,1 +1,38 @@
 
+function findAndSaveUser(Users) {
+  Users.findOne({}, (err, user) => {
+    if (err) {
+      return console.error(err);
+    }
+    user.name = 'zero';
+    user.save((err) => {
+      if (err) {
+        return console.error(err);
+      }
+      Users.findOne({ gender: 'm' }, (err, user) => {
+        
+      });
+    });
+  });
+}
+
+// 세번 중첩되어 코드의 깊어진 위의 코드를 아래와 같이 변경
+
+function findAndSaveUser(Users) {
+  Users.findOne({})
+  .then((user) => {
+    user.name = 'zero';
+    return user.save();
+  })
+  .then((user) => {
+    return Users.findOne({ gender: 'm'});
+  })
+  .then((user) => {
+    
+  })
+  .catch(err => {
+    console.error(err);
+  });
+}
+
+// findOne, save 메서드가 내부적으로 프로미스 객체를 가지고 있어서 
