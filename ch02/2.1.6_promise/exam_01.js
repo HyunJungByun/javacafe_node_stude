50c0e3506a00879be77fe7305087b7df9d87124e
HyunJungByun <wowyowowyo@gmail.com>
2018-09-17 13:20:41 +0900

    Update exam_01.js

--- ch02/2.1.6_promise/exam_01.js
+++ ch02/2.1.6_promise/exam_01.js
@@ -15,3 +15,24 @@ promise
 .catch((error) => {
   console.error(error);
 });
+
+// then 이나 catch 에서 다시 다른 then 이나 catch 붙일 수 있다.
+
+promise
+.then((message) => {
+  return new Promise((resolve, reject) => {
+    resolve(message);
+  });
+})
+.then((message2) => {
+  console.log(message2);
+  return new Promise((resolve, reject) => {
+    resolve(message2);
+  });
+})
+.then((message3) => {
+  console.log(message3);
+})
+.catch((error) => {
+  console.error(error);
+});
